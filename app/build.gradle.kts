e703ae6c912fb48cf35d5d09e39704ed6383505c
Kevinn183 <kevarebri@alu.edu.gva.es>
2023-11-14 19:49:04 +0100

    Firts fragments created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "es.kab.footballproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "es.kab.footballproject"
--- app/src/main/java/es/kab/footballproject/Activities/MainActivity.kt
+++ app/src/main/java/es/kab/footballproject/Activities/MainActivity.kt
@@ -1,11 +1,12 @@
-package es.kab.footballproject
+package es.kab.footballproject.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import es.kab.footballproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_login)
     }
 }
