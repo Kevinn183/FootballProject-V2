daf6de7ff4ad4cf14ff1fc939a4afa7dfc86e79d
Kevinn183 <kevarebri@alu.edu.gva.es>
2023-11-18 21:37:07 +0100

    Bottombar funcionalities added

--- app/src/main/java/es/kab/footballproject/Activities/MainActivity.kt
+++ app/src/main/java/es/kab/footballproject/Activities/MainActivity.kt
@@ -2,11 +2,68 @@ package es.kab.footballproject.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.navigation.NavigationBarView
+import es.kab.footballproject.Fragments.LoginFragment
+import es.kab.footballproject.Fragments.MakeFragment
+import es.kab.footballproject.Fragments.MundialFragment
+import es.kab.footballproject.Fragments.OroFragment
+import es.kab.footballproject.Fragments.TeamFragment
 import es.kab.footballproject.R
+import es.kab.footballproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNav.setOnItemSelectedListener(this)
     }
+
+
+
+    override fun onNavigationItemSelected(item:MenuItem) :Boolean{
+        return when(item.itemId){
+            R.id.item_liga ->{
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace<TeamFragment>(R.id.MainFragmentContainer)
+                }
+                true
+            }
+            R.id.item_mundial ->{
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace<MundialFragment>(R.id.MainFragmentContainer)
+                }
+                true
+            }
+            R.id.item_bdo ->{
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace<OroFragment>(R.id.MainFragmentContainer)
+                }
+                true
+            }
+            R.id.item_aline ->{
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace<MakeFragment>(R.id.MainFragmentContainer)
+                }
+                true
+            }
+            else -> false
+        }
+
+    }
+
+
+
+
 }
