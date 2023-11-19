111eb13b09f07bd1eeccf0c1b9da934c1b26c3a6
Kevinn183 <kevarebri@alu.edu.gva.es>
2023-11-19 21:51:29 +0100

    Make yor 11 in development

--- app/src/main/java/es/kab/footballproject/Fragments/MakeFragment.kt
+++ app/src/main/java/es/kab/footballproject/Fragments/MakeFragment.kt
@@ -1,60 +1,140 @@
 package es.kab.footballproject.Fragments
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import es.kab.footballproject.R
+import es.kab.footballproject.databinding.FragmentMakeBinding
+import es.kab.footballproject.databinding.FragmentOroBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class MakeFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MakeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MakeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentMakeBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var player1: TextView? = null
+    private var player2: TextView? = null
+    private var player3: TextView? = null
+    private var player4: TextView? = null
+    private var player5: TextView? = null
+    private var player6: TextView? = null
+    private var player7: TextView? = null
+    private var player8: TextView? = null
+    private var player9: TextView? = null
+    private var player10: TextView? = null
+    private var player11: TextView? = null
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_make, container, false)
-    }
+        binding = FragmentMakeBinding.inflate(inflater)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MakeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MakeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        player1 = binding.player1
+        player2 = binding.player2
+        player3 = binding.player3
+        player4 = binding.player4
+        player5 = binding.player5
+        player6 = binding.player6
+        player7 = binding.player7
+        player8 = binding.player8
+        player9 = binding.player9
+        player10 = binding.player10
+        player11= binding.player11
+
+        val cam1 = binding.cam1
+        val cam2 = binding.cam2
+        val cam3 = binding.cam3
+        val cam4 = binding.cam4
+        val cam5 = binding.cam5
+        val cam6 = binding.cam6
+        val cam7 = binding.cam7
+        val cam8 = binding.cam8
+        val cam9 = binding.cam9
+        val cam10 = binding.cam10
+        val cam11 = binding.cam11
+
+        cam1.setOnClickListener(this)
+        cam2.setOnClickListener(this)
+        cam3.setOnClickListener(this)
+        cam4.setOnClickListener(this)
+        cam5.setOnClickListener(this)
+        cam6.setOnClickListener(this)
+        cam7.setOnClickListener(this)
+        cam8.setOnClickListener(this)
+        cam9.setOnClickListener(this)
+        cam10.setOnClickListener(this)
+        cam11.setOnClickListener(this)
+
+        return binding.root
+         }
+
+    override fun onClick(v: View?) {
+//        val inflater = LayoutInflater.from(requireContext())
+//        val dialogView = inflater.inflate(R.layout.dialog_input, null)
+//        val editText : EditText= dialogView.findViewById(R.id.editTextDialogInput)
+//
+//        val alertDialogBuilder = AlertDialog.Builder(requireContext())
+//        alertDialogBuilder.setView(dialogView)
+
+//        val inflater = LayoutInflater.from(requireContext())
+//        val dialogView = inflater.inflate(R.layout.dialog_input, null)
+//        val editText = dialogView.findViewById<EditText>(R.id.editTextDialogInput)
+//
+//        val alertDialogBuilder = AlertDialog.Builder(requireContext())
+//        alertDialogBuilder.setView(dialogView)
+
+        if (v!=null){
+            when(v.id){
+                R.id.cam1 ->{
+                    Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam2 ->{
+                    Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam3 ->{
+                    Toast.makeText(context, "3", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam4 ->{
+                    Toast.makeText(context, "4", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam5 ->{
+                    Toast.makeText(context, "5", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam6 ->{
+                    Toast.makeText(context, "6", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam7 ->{
+                    Toast.makeText(context, "7", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam8 ->{
+                    Toast.makeText(context, "8", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam9 ->{
+                    Toast.makeText(context, "9", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam10 ->{
+                    Toast.makeText(context, "10", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cam11 ->{
+                    Toast.makeText(context, "11", Toast.LENGTH_SHORT).show()
                 }
             }
+
+        }
+
+
     }
+
+
+
 }
